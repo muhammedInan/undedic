4ac993416d92b84d1665842f184dd2b3aa2eb926
muhammedInan <muhammed-inan@outlook.com>
2021-01-28 20:28:38 +0100

    correction 2

--- src/Form/StudentType.php
+++ src/Form/StudentType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class StudentType extends AbstractType
 {
@@ -20,6 +21,7 @@ class StudentType extends AbstractType
 
                 ])
             ->add('numEtud')
+            ->add('appliquer', SubmitType::class)
         ;
     }
 
